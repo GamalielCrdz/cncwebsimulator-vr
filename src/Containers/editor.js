cd11d4b715416b821ed74f119df139abba7d110b
Gamaliel Castro <gamahappy98@gmail.com>
2020-01-13 19:46:32 -0600

    agregar simple marker en el editor
    Signed-off-by: Gamaliel Castro <gamahappy98@gmail.com>

--- src/Containers/editor.js
+++ src/Containers/editor.js
@@ -6,21 +6,35 @@ import AceEditor from "react-ace";
 import "ace-builds/src-noconflict/mode-gcode";
 import "ace-builds/src-noconflict/theme-xcode";
 
-export default (props) => {
-  const [toggle, setToggle] = useState(false);
-
+export default props => {
+  const [toggle, setToggle] = useState(true);
+  const markers = props.currentLine
+    ? [
+        {
+          startRow: props.currentLine,
+          startCol: 0,
+          endRow: props.currentLine,
+          endCol: 1,
+          className: "current-line",
+          type: "fullLine"
+        }
+      ]
+    : undefined;
   return (
     <EditorWrapper>
       <div className={`editor-container ${toggle && "show"}`}>
         <AceEditor
           mode="gcode"
           theme="xcode"
-          onChange={ (value) => { props.setEditorValue(value) }}
+          onChange={value => {
+            props.setEditorValue(value);
+          }}
           value={props.editorValue}
           //onChange={onChange}
-          name="UNIQUE_ID_OF_DIV"
+          name="GCODE-EDITOR"
           editorProps={{ $blockScrolling: true }}
           className={`editor-area`}
+          markers={markers}
         />
       </div>
       <Button
